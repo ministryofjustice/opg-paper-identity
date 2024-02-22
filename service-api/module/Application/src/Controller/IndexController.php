886c3eb7c229e9009cb11d790ea021dd2bdb5f6f
Faze Miah <faze.miah@digital.justice.gov.uk>
2024-02-22 11:15:22 +0000

    ID-27: add volumes

--- service-api/module/Application/src/Controller/IndexController.php
+++ service-api/module/Application/src/Controller/IndexController.php
@@ -11,6 +11,7 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-       return new JsonModel();
+        $data = array('Laminas' => 'Paper ID Service API');
+       return new JsonModel($data);
     }
 }
