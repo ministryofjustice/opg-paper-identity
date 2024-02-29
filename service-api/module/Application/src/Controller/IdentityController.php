f89198ce901d6fd915950efabfd3273b4363f3ad
Faze Miah <faze.miah@digital.justice.gov.uk>
2024-02-29 17:43:10 +0000

    ID-47: some simple API endpoint stubs to get front talking

---
+++ service-api/module/Application/src/Controller/IdentityController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Controller;
+
+use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\View\Model\JsonModel;
+
+class IdentityController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new JsonModel();
+    }
+
+    public function createAction()
+    {
+    }
+
+    public function methodAction()
+    {
+        $data = [
+            'Passport',
+            'Driving Licence',
+            'National Insurance Number'
+        ];
+
+        return new JsonModel($data);
+    }
+
+    public function detailsAction()
+    {
+        $data = [
+            'Name' => 'Mary Anne Chapman',
+            'DOB' => '01 May 1943',
+            'Address' => 'Address line 1, line 2, Country, BN1 4OD',
+            'Role' => 'Donor',
+            'LPA' => ['PA M-1234-ABCB-XXXX', 'PW M-1234-ABCD-AAAA']
+        ];
+
+        return new JsonModel($data);
+    }
+}
+
