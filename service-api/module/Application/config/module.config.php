0995e9565fd137a87e0179ceeffe5117a88edfbe
Faze Miah <faze.miah@digital.justice.gov.uk>
2024-03-05 16:15:02 +0000

    ID-45: fix log errors, add views back as per Laminas API Tools

--- service-api/module/Application/config/module.config.php
+++ service-api/module/Application/config/module.config.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Application;
 
+use Application\Aws\DynamoDbClientFactory;
+use Aws\DynamoDb\DynamoDbClient;
+use Behat\Testwork\Cli\Application;
 use Laminas\Router\Http\Literal;
 use Laminas\Router\Http\Segment;
 use Laminas\ServiceManager\Factory\InvokableFactory;
+use Utilities\PopulateDynomoData;
 
 return [
     'router' => [
@@ -51,6 +55,16 @@ return [
                     ],
                 ],
             ],
+            'testdata' => [
+                'type'    => Literal::class,
+                'options' => [
+                    'route'    => '/identity/testdata',
+                    'defaults' => [
+                        'controller' => Controller\IdentityController::class,
+                        'action'     => 'testdata',
+                    ],
+                ],
+            ],
         ],
     ],
     'controllers' => [
@@ -60,9 +74,29 @@ return [
         ],
     ],
 
+    'service_manager' => [
+        'invokables' => [
+
+        ],
+        'factories' => [
+            DynamoDbClient::class => DynamoDbClientFactory::class,
+        ],
+    ],
+
     'view_manager' => [
-        'strategies' => [
-            'ViewJsonStrategy',
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map'             => [
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ],
+        'template_path_stack'      => [
+            __DIR__ . '/../view',
         ],
     ],
 ];
