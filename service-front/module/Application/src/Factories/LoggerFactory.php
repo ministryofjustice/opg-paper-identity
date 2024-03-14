2cbd720aa54b062fe68f9775d9afa3943d8f9de8
Greg Tyler <gregtyler@users.noreply.github.com>
2024-03-14 09:11:51 +0000

    Add logger, auto-log response exceptions (#42)
    If an exception is thrown in the controller it is absorbed and an error template is shown. Since we don't want to show the details of the error in the user-facing template, we need to explicitly log it during the EVENT_FINISH event.
    
    Sets up monolog with standard OPG logging output under the hood, but can just be fetched as LoggerInterface anywhere in code.
    
    Duplication due to setting this up in both API and Frontend.
    
    Fixes ID-65 #minor

---
+++ service-front/module/Application/src/Factories/LoggerFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Factories;
+
+use Application\Services\Logging\OpgFormatter;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Psr\Container\ContainerInterface;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+
+class LoggerFactory implements FactoryInterface
+{
+    /**
+     * @param ContainerInterface $container
+     * @param string                          $requestedName
+     * @param array<mixed>|null               $options
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null): LoggerInterface
+    {
+        $streamHandler = new StreamHandler('php://stderr', LogLevel::INFO);
+        $streamHandler->setFormatter(new OpgFormatter());
+
+        return new Logger('opg-paper-identity/front', [$streamHandler]);
+    }
+}
