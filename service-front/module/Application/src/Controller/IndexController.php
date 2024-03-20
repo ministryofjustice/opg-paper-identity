502d83487d80897d843d66890b7f74069186b9be
John Riordan <jr@spinhalf.com>
2024-03-20 09:29:20 +0000

    PR fixes

--- service-front/module/Application/src/Controller/IndexController.php
+++ service-front/module/Application/src/Controller/IndexController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Application\Controller;
 
 use Application\Contracts\OpgApiServiceInterface;
+use Application\Forms\DrivingLicenceNumber;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
 use Application\Forms\NationalInsuranceNumber;
@@ -88,5 +89,37 @@ class IndexController extends AbstractActionController
         }
 
         return $view->setTemplate('application/pages/national_insurance_number');
+    }
+
+    public function drivingLicenceNumberAction(): ViewModel
+    {
+        $view = new ViewModel();
+
+        $form = (new AttributeBuilder())->createForm(DrivingLicenceNumber::class);
+        $detailsData = $this->opgApiService->getDetailsData();
+
+        $view->setVariable('details_data', $detailsData);
+        $view->setVariable('form', $form);
+
+        if (count($this->getRequest()->getPost())) {
+            $formData = $this->getRequest()->getPost();
+            $form->setData($formData);
+            $validFormat = $form->isValid();
+
+            if ($validFormat) {
+                $view->setVariable('dln_data', $formData);
+                /**
+                 * @psalm-suppress InvalidArrayAccess
+                 */
+                $validDln = $this->opgApiService->checkDlnValidity($formData['dln']);
+                if ($validDln) {
+                    return $view->setTemplate('application/pages/driving_licence_number_success');
+                } else {
+                    return $view->setTemplate('application/pages/driving_licence_number_fail');
+                }
+            }
+        }
+
+        return $view->setTemplate('application/pages/driving_licence_number');
     }
 }
