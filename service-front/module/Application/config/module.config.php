ae5f1e7b7e734e4b877cd1d87dfe164acedbaa75
John Riordan <jr@spinhalf.com>
2024-03-22 13:02:42 +0000

    merge conflict fix

--- service-front/module/Application/config/module.config.php
+++ service-front/module/Application/config/module.config.php
@@ -71,6 +71,9 @@ return [
             Controller\IndexController::class => LazyControllerAbstractFactory::class,
         ],
     ],
+    'listeners' => [
+        AuthListener::class
+    ],
     'view_manager' => [
         'display_not_found_reason' => true,
         'display_exceptions'       => true,
