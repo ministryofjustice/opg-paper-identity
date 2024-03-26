96ade226f707fdd2bca8f3adf5f71cf9ab4fd8ee
Faze Miah <159784983+fazemiah-moj@users.noreply.github.com>
2024-03-26 16:46:35 +0000

    ID-45-Propulate-DynamoDb-with-initial-data-for-ID-service (#30)
    * ID-45: fix log errors, add views back as per Laminas API Tools
    
    * ID-45: add localstack, config and dynamodb local config
    
    * ID-45: need to add GSI to be able to query by attributes other than ID, basic working import and query dataset in dynamodb
    
    * ID-45: tidy up
    
    * ID-45: simplify importHandler and add controller factory to inject service if we need to useit
    
    * ID-45: fix second index not being created on init script
    
    * Clean up
    
    * Remove duplicate json file
    
    * Remove API tools src folder
    
    * PHPCS fixes
    
    * Fix some psalm issues
    
    * Fixing rest of psalm errors and change error level to 2
    
    * Fix phpcs errors
    
    * Merge main into branch
    
    * Update service-api/config/autoload/aws.global.php
    
    Co-authored-by: Greg Tyler <gregtyler@users.noreply.github.com>
    
    * Update docker-compose.yml
    
    Co-authored-by: Greg Tyler <gregtyler@users.noreply.github.com>
    
    * Update docker-compose.yml
    
    Co-authored-by: Greg Tyler <gregtyler@users.noreply.github.com>
    
    * ID-45: remove duplicate service_manager from config, remove unused di.global and use name from request
    
    * ID-45: clean up env files
    
    * ID-45: add new line eof
    
    * ID-45: fix psalm error on request interface confusion
    
    * Fix same psalm issue again
    
    ---------
    
    Co-authored-by: Greg Tyler <greg.tyler@digital.justice.gov.uk>
    Co-authored-by: Greg Tyler <gregtyler@users.noreply.github.com>

---
+++ service-api/module/Application/src/Aws/DynamoDbClientFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Aws;
+
+use Aws\DynamoDb\DynamoDbClient;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Psr\Container\ContainerInterface;
+use Telemetry\Middleware\Aws as MiddlewareAws;
+
+class DynamoDbClientFactory implements FactoryInterface
+{
+    /**
+     * @param string $requestedName
+     * @param null|array<mixed> $options
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null): DynamoDbClient
+    {
+        /** @var array{"aws": array<string, string|bool>} $config */
+        $config = $container->get('Config');
+
+        $dynamoDbClient = new DynamoDbClient($config['aws']);
+
+        return $dynamoDbClient;
+    }
+}
