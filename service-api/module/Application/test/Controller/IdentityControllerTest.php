8344fa5b55e0411924a6dea3bbfbb31df0ca7fd7
John Riordan <jr@spinhalf.com>
2024-04-05 12:50:55 +0100

    fixing tests

--- service-api/module/Application/test/Controller/IdentityControllerTest.php
+++ service-api/module/Application/test/Controller/IdentityControllerTest.php
@@ -114,8 +114,8 @@ class IdentityControllerTest extends AbstractHttpControllerTestCase
         return [
             ['CHAPM301534MA9AY', 'PASS', Response::STATUS_CODE_200],
             ['SMITH710238HA3DY', 'PASS', Response::STATUS_CODE_200],
-            ['SMITH720238HA3DX', 'NO_MATCH', Response::STATUS_CODE_200],
-            ['JONES630536AB3JZ', 'NOT_ENOUGH_DETAILS', Response::STATUS_CODE_200]
+            ['SMITH720238HA3D8', 'NO_MATCH', Response::STATUS_CODE_200],
+            ['JONES630536AB3J9', 'NOT_ENOUGH_DETAILS', Response::STATUS_CODE_200]
         ];
     }
 
