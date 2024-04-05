2b3edc0ca0605cccd0af3326db2b7a4116c3f461
Faze Miah <faze.miah@digital.justice.gov.uk>
2024-04-05 10:38:23 +0100

    ID-100: tidy nino mock to be more consistent

--- service-api/module/Application/test/Controller/IdentityControllerTest.php
+++ service-api/module/Application/test/Controller/IdentityControllerTest.php
@@ -67,11 +67,12 @@ class IdentityControllerTest extends AbstractHttpControllerTestCase
     /**
      * @dataProvider ninoData
      */
-    public function testNino(string $nino, int $status): void
+    public function testNino(string $nino, string $response, int $status): void
     {
         $this->dispatch('/identity/validate_nino', 'POST', ['nino' => $nino]);
         $this->assertResponseStatusCode($status);
         $this->assertModuleName('application');
+        $this->assertEquals('{"status":"' . $response . '"}', $this->getResponse()->getContent());
         $this->assertControllerName(IdentityController::class); // as specified in router's controller name alias
         $this->assertControllerClass('IdentityController');
         $this->assertMatchedRouteName('validate_nino');
@@ -80,10 +81,10 @@ class IdentityControllerTest extends AbstractHttpControllerTestCase
     public static function ninoData(): array
     {
         return [
-            ['AA112233A', Response::STATUS_CODE_200],
-            ['BB112233A', Response::STATUS_CODE_200],
-            ['AA112233D', Response::STATUS_CODE_400],
-            ['AA112233C', Response::STATUS_CODE_400]
+            ['AA112233A', 'PASS', Response::STATUS_CODE_200],
+            ['BB112233A', 'PASS', Response::STATUS_CODE_200],
+            ['AA112233D', 'NOT_ENOUGH_DETAILS', Response::STATUS_CODE_200],
+            ['AA112233C', 'NO_MATCH', Response::STATUS_CODE_200]
         ];
     }
 
