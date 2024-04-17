f413a8367afe1b67b18ca2120f112cadb3c1d83d
Faze Miah <faze.miah@digital.justice.gov.uk>
2024-04-17 10:00:48 +0100

    ID-82: KBV questions mock

---
+++ service-api/module/Application/src/KBV/KBVServiceInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\KBV;
+
+interface KBVServiceInterface
+{
+    /**
+     * @return array{
+     *  question: string,
+     *  prompts: string[],
+     *  answer: string
+     * }[]
+     */
+    public function getKBVQuestions(): array;
+}
---
+++ service-api/module/Application/src/Mock/KBV/KBVService.php
@@ -0,0 +1,110 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Mock\KBV;
+
+use Application\KBV\KBVServiceInterface;
+
+class KBVService implements KBVServiceInterface
+{
+    public function getKBVQuestions(): array
+    {
+        $questionsList = $this->questionsList();
+        $questionSelection = [];
+
+        foreach (array_rand($questionsList, 4) as $key) {
+            $question = $questionsList[$key];
+            shuffle($question['prompts']);
+            $questionSelection[] = $question;
+        }
+
+        return $questionSelection;
+    }
+
+    private function questionsList(): array
+    {
+        return [
+                0 => [
+                    'question' => 'Who is your electricity supplier?',
+                    'prompts' => [
+                        0 => 'VoltWave',
+                        1 => 'Glow Electric',
+                        2 => 'Powergrid Utilities',
+                        3 => 'Bright Bristol Power'
+                    ],
+                    'answer' => 'VoltWave'
+                ],
+                1 => [
+                    'question' => 'How much was your last phone bill?',
+                    'prompts' => [
+                        0 => '£5.99',
+                        1 => '£11',
+                        2 => '£16.84',
+                        3 => '£1.25'
+                    ],
+                    'answer' => '£5.99'
+                ],
+                2 => [
+                    'question' => 'What is your mother’s maiden name?',
+                    'prompts' => [
+                        0 => 'Germanotta',
+                        1 => 'Gumm',
+                        2 => 'Micklewhite',
+                        3 => 'Blythe'
+                    ],
+                    'answer' => 'Germanotta'
+                ],
+                3 => [
+                    'question' => 'What are the last two characters of your car number plate??',
+                    'prompts' => [
+                        0 => 'IF',
+                        1 => 'SJ',
+                        2 => 'WP',
+                        3 => 'PG'
+                    ],
+                    'answer' => 'IF'
+                ],
+                4 => [
+                    'question' => 'Name one of your current account providers',
+                    'prompts' => [
+                        0 => 'Liberty Trust Bank',
+                        1 => 'Heritage Horizon Bank',
+                        2 => 'Prosperity Peak Financial',
+                        3 => 'Summit State Saving'
+                    ],
+                    'answer' => 'Liberty Trust Bank'
+                ],
+                5 => [
+                    'question' => 'In what month did you move into your current house?',
+                    'prompts' => [
+                        0 => 'July',
+                        1 => 'September',
+                        2 => 'March',
+                        3 => 'April'
+                    ],
+                    'answer' => 'July'
+                ],
+                6 => [
+                    'question' => 'Which company provides your car insurance?',
+                    'prompts' => [
+                        0 => 'SafeDrive Insurance',
+                        1 => 'Guardian Drive Assurance',
+                        2 => 'SheildSafe',
+                        3 => 'Swift Cover Protection'
+                    ],
+                    'answer' => 'SafeDrive Insurance'
+                ],
+                7 => [
+                    'question' => 'What colour is your front door?',
+                    'prompts' => [
+                        0 => 'Pink',
+                        1 => 'Green',
+                        2 => 'Black',
+                        3 => 'Yellow'
+                    ],
+                    'answer' => 'Pink'
+                ],
+        ];
+    }
+}
