05740c6bf2146ba20aa41aa9a454e6506c6c23f7
John Riordan <jr@spinhalf.com>
2024-04-19 17:14:00 +0100

    phpcs fix

--- service-api/module/Application/src/Controller/IdentityController.php
+++ service-api/module/Application/src/Controller/IdentityController.php
@@ -324,7 +324,8 @@ class IdentityController extends AbstractActionController
             case 'M-0000-0000-0003':
                 $status = Response::STATUS_CODE_400;
                 $message = "This LPA cannot be added to this ID check because the 
-                certificate provider details on this LPA do not match.   Edit the certificate provider record in Sirius if appropriate and find again.";
+                certificate provider details on this LPA do not match.   
+                Edit the certificate provider record in Sirius if appropriate and find again.";
                 $response['additional_data'] = [
                     'Name' => 'John Brian Adams',
                     'Address' => [
