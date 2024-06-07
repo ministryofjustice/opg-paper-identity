b5b02d4f16e36c63075f4953dcedb5f418d40e15
Greg Tyler <gregtyler@users.noreply.github.com>
2024-06-07 13:01:17 +0100

    Add Yoti secrets to localstack (#117)
    And pass the prefix into the API service.
    
    Also, remove the unused `PAPER_ID_BACK_AWS_DEBUG` env var
    
    For ID-116 #patch

--- scripts/localstack/init/localstack_init.sh
+++ scripts/localstack/init/localstack_init.sh
@@ -7,3 +7,11 @@ awslocal dynamodb create-table \
   --key-schema \
     AttributeName=id,KeyType=HASH \
   --provisioned-throughput ReadCapacityUnits=1000,WriteCapacityUnits=1000
+
+awslocal secretsmanager create-secret --name local/paper-identity/yoti/certificate \
+    --description "PEM certificate for authentication with Yoti API" \
+    --secret-string "empty"
+
+awslocal secretsmanager create-secret --name local/paper-identity/yoti/sdk-client-id \
+    --description "ID of Yoti client" \
+    --secret-string "empty"
