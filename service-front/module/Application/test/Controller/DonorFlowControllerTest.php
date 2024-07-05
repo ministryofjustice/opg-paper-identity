d499030666b0745329632b9984d5ea0b47cbf8b5
John Riordan <jr@spinhalf.com>
2024-07-05 14:50:53 +0100

    Feature/id 139 remove lpa stub api method (#127)
    * Draft LPA helper class
    
    * cs fixes
    
    * added find lpa endpoint to sirius
    
    * working sirius vs opg id check + tests
    
    * WIP
    
    * adding new dynamoDb index
    
    * changed opg looup to case
    
    * refactored helpers + tests
    
    * psalm / cs fixes
    
    * more tests
    
    * working  find lpa + tests
    
    * added not found test for LPA
    
    * fixing error on no match test
    
    * front end working for no match and status errors
    
    * removing case data from redundant check
    
    * fixing typo
    
    * imposter
    
    * wIP
    
    * update sirius mock
    
    * WIP
    
    * WIP
    
    * remove imposter
    
    * add external port
    
    * openapi spec updated to include custom responses
    
    * restoring docker compose
    
    * working tests and web flow
    
    * fixing docker complse
    
    * Enabling button on success
    
    * fixing typo
    
    * PR fixes
    
    * refactored LAP response DTO
    
    * refactored add lpa screen for new DTO
    
    * PR fixes
    
    * renaming status field
    
    * refactoring lpa processing logic
    
    * removing test data from yml
    
    * refactor to private methods
    
    * refactoring response structure
    
    * fixing view file
    
    * psalm / cs
    
    * PR fixes
    
    * PR feedback
    
    * phpcs

--- service-front/module/Application/test/Controller/DonorFlowControllerTest.php
+++ service-front/module/Application/test/Controller/DonorFlowControllerTest.php
@@ -43,7 +43,7 @@ class DonorFlowControllerTest extends AbstractHttpControllerTestCase
             "Name" => "Mary Anne Chapman",
             "DOB" => "01 May 1943",
             "Address" => "Address line 1, line 2, Country, BN1 4OD",
-            "Role" => "Donor",
+            "Role" => "donor",
             "LPA" => [
                 "PA M-1234-ABCB-XXXX",
                 "PW M-1234-ABCD-AAAA"
@@ -85,7 +85,7 @@ class DonorFlowControllerTest extends AbstractHttpControllerTestCase
             "Name" => "Mary Anne Chapman",
             "DOB" => "01 May 1943",
             "Address" => "Address line 1, line 2, Country, BN1 4OD",
-            "Role" => "Donor",
+            "Role" => "donor",
             "LPA" => [
                 "PA M-1234-ABCB-XXXX",
                 "PW M-1234-ABCD-AAAA"
@@ -153,7 +153,7 @@ class DonorFlowControllerTest extends AbstractHttpControllerTestCase
             "Name" => "Mary Anne Chapman",
             "DOB" => "01 May 1943",
             "Address" => "Address line 1, line 2, Country, BN1 4OD",
-            "Role" => "Donor",
+            "Role" => "donor",
             "LPA" => [
                 "PA M-1234-ABCB-XXXX",
                 "PW M-1234-ABCD-AAAA"
@@ -181,7 +181,7 @@ class DonorFlowControllerTest extends AbstractHttpControllerTestCase
             "Name" => "Mary Anne Chapman",
             "DOB" => "01 May 1943",
             "Address" => "Address line 1, line 2, Country, BN1 4OD",
-            "Role" => "Donor",
+            "Role" => "donor",
             "LPA" => [
                 "PA M-1234-ABCB-XXXX",
                 "PW M-1234-ABCD-AAAA"
@@ -209,7 +209,7 @@ class DonorFlowControllerTest extends AbstractHttpControllerTestCase
             "Name" => "Mary Anne Chapman",
             "DOB" => "01 May 1943",
             "Address" => "Address line 1, line 2, Country, BN1 4OD",
-            "Role" => "Donor",
+            "Role" => "donor",
             "LPA" => [
                 "PA M-1234-ABCB-XXXX",
                 "PW M-1234-ABCD-AAAA"
@@ -237,7 +237,7 @@ class DonorFlowControllerTest extends AbstractHttpControllerTestCase
             "Name" => "Mary Anne Chapman",
             "DOB" => "01 May 1943",
             "Address" => "Address line 1, line 2, Country, BN1 4OD",
-            "Role" => "Donor",
+            "Role" => "donor",
             "LPA" => [
                 "PA M-1234-ABCB-XXXX",
                 "PW M-1234-ABCD-AAAA"
@@ -282,7 +282,7 @@ class DonorFlowControllerTest extends AbstractHttpControllerTestCase
             "Name" => "Mary Anne Chapman",
             "DOB" => "01 May 1943",
             "Address" => "Address line 1, line 2, Country, BN1 4OD",
-            "Role" => "Donor",
+            "Role" => "donor",
             "LPA" => [
                 "PA M-1234-ABCB-XXXX",
                 "PW M-1234-ABCD-AAAA"
@@ -346,7 +346,7 @@ class DonorFlowControllerTest extends AbstractHttpControllerTestCase
             "Name" => "Mary Anne Chapman",
             "dob" => "01 May 1943",
             "Address" => "Address line 1, line 2, Country, BN1 4OD",
-            "Role" => "Donor",
+            "Role" => "donor",
             "LPA" => [
                 "PA M-1234-ABCB-XXXX",
                 "PW M-1234-ABCD-AAAA"
