e749d7a4092c26fb2175e5e0d4746636dd913586
John Riordan <jr@spinhalf.com>
2024-07-11 17:50:32 +0100

    ID-205 remove whitespace phpcs #patch

--- service-front/module/Application/src/Controller/DonorPostOfficeFlowController.php
+++ service-front/module/Application/src/Controller/DonorPostOfficeFlowController.php
@@ -137,7 +137,7 @@ class DonorPostOfficeFlowController extends AbstractActionController
         $locationForm = (new AttributeBuilder())->createForm(PostOfficeSearchLocation::class);
         $view->setVariable('form', $form);
         $view->setVariable('location_form', $locationForm);
-        
+
         if (! isset($detailsData['searchPostcode'])) {
             $searchPostcode = $detailsData['address']['postcode'];
         } else {
