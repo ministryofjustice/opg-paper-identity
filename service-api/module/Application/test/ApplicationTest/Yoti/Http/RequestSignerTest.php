78d879c4b567d293c34b8d3ac33004e0b866c8ca
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2024-08-09 11:09:15 +0100

    Run test suites without dependencies
    PHPCS, Psalm and PHPUnit tests should not require any dependencies to run, so should be run in `--no-deps` mode.
    
    I rewrote `RequestSignerTest` to generate the SSL key in PHP rather than doing so via localstack.
    
    `front-unit-test` still needs to be run with dependencies to use Pact, but we'll be able to internalise that when Pact v10 is released.
    
    #patch

--- service-api/module/Application/test/ApplicationTest/Yoti/Http/RequestSignerTest.php
+++ service-api/module/Application/test/ApplicationTest/Yoti/Http/RequestSignerTest.php
@@ -7,30 +7,31 @@ namespace ApplicationTest\Yoti\Http;
 use Application\Aws\Secrets\AwsSecret;
 use Application\Yoti\Http\Exception\YotiAuthException;
 use Application\Yoti\Http\RequestSigner;
-use Aws\Result;
-use Aws\SecretsManager\SecretsManagerClient;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class RequestSignerTest extends TestCase
 {
     private AwsSecret|MockObject $pemFileMock;
-    private SecretsManagerClient $secretsManagerClient;
-    private Result $privateResult;
-    private const PATH = '/api/endpoint';
-    private const METHOD = 'POST';
+    private static string $pemKeyPrivate;
+    private static string $pemKeyPublic;
+    private const string PATH = '/api/endpoint';
+    private const string METHOD = 'POST';
     private RequestSigner $sut;
+
+    public static function setUpBeforeClass(): void
+    {
+        $privateKey = openssl_pkey_new(['private_key_bits' => 2048]);
+        self::$pemKeyPublic = openssl_pkey_get_details(openssl_pkey_get_private($privateKey))['key'];
+
+        openssl_pkey_export(openssl_pkey_get_private($privateKey), $privateKeyContents);
+        self::$pemKeyPrivate = $privateKeyContents;
+    }
+
+
     public function setUp(): void
     {
         $this->pemFileMock = $this->createMock(AwsSecret::class);
-
-        $this->secretsManagerClient = new SecretsManagerClient([
-            'endpoint' => getenv('SECRETS_MANAGER_ENDPOINT'),
-            'region' => 'eu-west-1'
-        ]);
-        $this->privateResult = $this->secretsManagerClient->getSecretValue([
-            'SecretId' => 'local/paper-identity/yoti/private-key',
-        ]);
 
         $this->sut = new RequestSigner();
     }
@@ -46,7 +47,7 @@ class RequestSignerTest extends TestCase
 
         $this->pemFileMock->expects($this->atLeastOnce())
             ->method("getValue")
-            ->willReturn($this->privateResult['SecretString']);
+            ->willReturn(self::$pemKeyPrivate);
 
         $signedMessage = $this->sut->generateSignature(
             self::PATH,
@@ -55,12 +56,8 @@ class RequestSignerTest extends TestCase
             $payload
         );
         $messageToSign = self::METHOD . '&' . self::PATH . '&' . base64_encode($payload);
-        /** @var array $publicKeyResult */
-        $publicKeyResult = $this->secretsManagerClient->getSecretValue([
-            'SecretId' => 'local/paper-identity/yoti/public-key',
-        ]);
 
-        $publicKey = openssl_pkey_get_public($publicKeyResult['SecretString']);
+        $publicKey = openssl_pkey_get_public(self::$pemKeyPublic);
 
         $verify = openssl_verify($messageToSign, base64_decode($signedMessage), $publicKey, OPENSSL_ALGO_SHA256);
 
@@ -74,7 +71,7 @@ class RequestSignerTest extends TestCase
     {
         $this->pemFileMock->expects($this->atLeastOnce())
             ->method("getValue")
-            ->willReturn($this->privateResult["SecretString"]);
+            ->willReturn(self::$pemKeyPrivate);
 
         // Generate signature
         $signature = $this->sut->generateSignature(
