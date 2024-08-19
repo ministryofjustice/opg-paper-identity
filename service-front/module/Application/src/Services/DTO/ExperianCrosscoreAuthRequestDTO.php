bb355743ce5132eb548eb169f98a8a80d3acb859
John Riordan <jr@spinhalf.com>
2024-08-19 18:02:31 +0100

    ID-275 adding service, DTOs#minor

---
+++ service-front/module/Application/src/Services/DTO/ExperianCrosscoreAuthRequestDTO.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Services\DTO;
+
+class
+ExperianCrosscoreAuthRequestDTO
+{
+    public function __construct(
+        private readonly string $userName,
+        private readonly string $password,
+        private readonly string $clientId,
+        private readonly string $clientSecret
+    ) {
+
+    }
+
+    public function userName(): string
+    {
+        return $this->userName;
+    }
+
+    public function password(): string
+    {
+        return $this->password;
+    }
+
+    public function clientId(): string
+    {
+        return $this->clientId;
+    }
+
+    public function clientSecret(): string
+    {
+        return $this->clientSecret;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            $this->userName,
+            $this->password,
+            $this->clientId,
+            $this->clientSecret,
+            $this->clientSecret,
+        ];
+    }
+}
---
+++ service-front/module/Application/src/Services/DTO/ExperianCrosscoreAuthResponseDTO.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Services\DTO;
+
+class ExperianCrosscoreAuthResponseDTO
+{
+    public function __construct(
+        private readonly string $accessToken,
+        private readonly string $refreshToken,
+        private readonly string $issuedAt,
+        private readonly string $expiresIn,
+        private readonly string $tokenType,
+    ) {
+
+    }
+
+    public function accessToken(): string
+    {
+        return $this->accessToken;
+    }
+
+    public function refreshToken(): string
+    {
+        return $this->refreshToken;
+    }
+
+    public function issuedAt(): string
+    {
+        return $this->issuedAt;
+    }
+
+    public function expiresIn(): string
+    {
+        return $this->expiresIn;
+    }
+
+    public function tokenType(): string
+    {
+        return $this->tokenType;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            $this->accessToken,
+            $this->refreshToken,
+            $this->issuedAt,
+            $this->expiresIn,
+            $this->tokenType,
+        ];
+    }
+}
