ec64408a945e6b6ddcbed9246d12069b7a3498ef
John Riordan <jr@spinhalf.com>
2024-08-28 17:28:52 +0100

    ID-278 adding cache handler#minor

---
+++ service-api/module/Application/src/Cache/ApcHelper.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Cache;
+
+class ApcHelper
+{
+    /**
+     * @throws \Exception
+     */
+    public function __construct()
+    {
+        if (!function_exists('apcu_enabled') && apcu_enabled()) {
+            throw new \Exception("APCU Cache is not available.");
+        }
+    }
+
+    public function getValue(string $index): mixed
+    {
+        return apcu_fetch($index);
+    }
+
+    public function setValue(string $index, mixed $value): bool
+    {
+        return apcu_store($index, $value);
+    }
+}
