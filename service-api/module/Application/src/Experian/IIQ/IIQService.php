9b504eb08e491b81e136465598d9a613ab2169df
Faze Miah <faze.miah@digital.justice.gov.uk>
2024-09-11 14:32:36 +0100

    ID-259: correct response structure access?

--- service-api/module/Application/src/Experian/IIQ/IIQService.php
+++ service-api/module/Application/src/Experian/IIQ/IIQService.php
@@ -65,16 +65,25 @@ class IIQService
                 ]
             ]);
 
-            if ($request->SAAResult->Results->Outcome !== 'Authentication Questions returned') {
-                $this->logger->error($request->SAAResult->Results->Outcome);
-                throw new CannotGetQuestionsException("Error retrieving questions");
-            }
-            if ($request->SAAResult->Results->NextTransId->string !== 'RTQ') {
-                $this->logger->error($request->SAAResult->Results->NextTransId->string);
-                throw new CannotGetQuestionsException("Error retrieving questions");
+            //@todo remove this after debugging
+            $this->logger->info($request->SAAResponse->SAAResult->Control->URN);
+            $this->logger->info($request->SAAResult->Control->URN);
+
+
+            if ($request->SAAResponse->SAAResult) {
+                if ($request->SAAResponse->SAAResult->Results->Outcome !== 'Authentication Questions returned') {
+                    $this->logger->error($request->SAAResponse->SAAResult->Results->Outcome);
+                    throw new CannotGetQuestionsException("Error retrieving questions");
+                }
+                if ($request->SAAResponse->SAAResult->Results->NextTransId->string !== 'RTQ') {
+                    $this->logger->error($request->SAAResponse->SAAResult->Results->NextTransId->string);
+                    throw new CannotGetQuestionsException("Error retrieving questions");
+                }
+            } else {
+                throw new CannotGetQuestionsException("No results");
             }
 
-            return (array)$request->SAAResult->Questions->Question;
+            return (array)$request->SAAResponse->SAAResult->Questions->Question;
         });
     }
 }
