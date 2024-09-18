4b1608bcf12ca04dfbaa66ccadfddce9ef9e2ac2
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2024-09-18 15:21:56 +0100

    Update IIQ mock
    It now handles pass/fail and setting more questions, following the same logic as the real API. It continues to use the mock questions from ID-82.
    
    Also adds KBV pages to the happy path end-to-end journeys and adds a KBV failure end-to-end journey.
    
    Fixes ID-346 #patch

--- e2e-tests/cypress/e2e/donor/donor-journey.cy.ts
+++ e2e-tests/cypress/e2e/donor/donor-journey.cy.ts
@@ -18,5 +18,49 @@ describe("Identify a Donor", () => {
 
     cy.contains("Initial identity confirmation complete");
     cy.get(".govuk-button").contains("Continue").click();
+
+    cy.contains("Select answer");
+
+    cy.selectKBVAnswer({ correct: true });
+    cy.get(".govuk-button").contains("Continue").click();
+
+    cy.selectKBVAnswer({ correct: true });
+    cy.get(".govuk-button").contains("Continue").click();
+
+    cy.selectKBVAnswer({ correct: true });
+    cy.get(".govuk-button").contains("Continue").click();
+
+    cy.contains(".moj-banner", "Identity check passed");
+  });
+
+  it("fails if you get KBVs wrong", () => {
+    cy.visit("/start?personType=donor&lpas[]=M-1234-1234-1234");
+
+    cy.contains("How will they confirm their identity?");
+    cy.contains("National Insurance number").click();
+    cy.get(".govuk-button").contains("Continue").click();
+
+    cy.contains("Do the details match the ID document?");
+    cy.get(".govuk-button").contains("Continue").click();
+
+    cy.contains("Which LPAs should this identity check apply to?");
+    cy.get(".govuk-button").contains("Continue").click();
+
+    cy.contains("National insurance number");
+    cy.getInputByLabel("National Insurance number").type("AA 12 34 56 A");
+    cy.get(".govuk-button").contains("Continue").click();
+
+    cy.contains("Initial identity confirmation complete");
+    cy.get(".govuk-button").contains("Continue").click();
+
+    cy.contains("Select answer");
+
+    cy.selectKBVAnswer({ correct: false });
+    cy.get(".govuk-button").contains("Continue").click();
+
+    cy.selectKBVAnswer({ correct: false });
+    cy.get(".govuk-button").contains("Continue").click();
+
+    cy.contains(".moj-banner", "Identity check failed");
   });
 });
