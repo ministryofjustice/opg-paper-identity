7c837d0c2d9935eb61d7cab296f60460082f0183
David Bridgwood <david.bridgwood@digital.justice.gov.uk>
2024-10-11 14:22:34 +0100

    update URL used for edit link to sirius and add to CP route

--- service-front/module/Application/src/Controller/Factory/CPFlowControllerFactory.php
+++ service-front/module/Application/src/Controller/Factory/CPFlowControllerFactory.php
@@ -4,35 +4,35 @@ declare(strict_types=1);
 
 namespace Application\Controller\Factory;
 
-use Application\Config\Config;
 use Application\Contracts\OpgApiServiceInterface;
-use Application\Controller\PostOfficeFlowController;
+use Application\Controller\CPFlowController;
 use Application\Helpers\FormProcessorHelper;
+use Application\Helpers\AddressProcessorHelper;
+use Application\Helpers\LpaFormHelper;
 use Application\PostOffice\DocumentTypeRepository;
 use Application\Services\SiriusApiService;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Psr\Container\ContainerInterface;
 
-class PostOfficeFlowControllerFactory implements FactoryInterface
+class CPFlowControllerFactory implements FactoryInterface
 {
     /**
      * @param ContainerInterface $container
      * @param string $requestedName
      * @param mixed[]|null $options
      */
-    public function __invoke(
-        ContainerInterface $container,
-        $requestedName,
-        array $options = null
-    ): PostOfficeFlowController {
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null): CPFlowController
+    {
         /** @var string $siriusPublicUrl */
         $siriusPublicUrl = getenv("SIRIUS_PUBLIC_URL");
         $config = $container->get('Config');
 
-        return new PostOfficeFlowController(
+        return new CPFlowController(
             $container->get(OpgApiServiceInterface::class),
             $container->get(FormProcessorHelper::class),
             $container->get(SiriusApiService::class),
+            $container->get(AddressProcessorHelper::class),
+            $container->get(LpaFormHelper::class),
             $container->get(DocumentTypeRepository::class),
             $config,
             $siriusPublicUrl
