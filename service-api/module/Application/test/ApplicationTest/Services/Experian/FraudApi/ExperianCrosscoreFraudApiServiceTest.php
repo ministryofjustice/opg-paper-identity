f096bfc70864dd3632cacd8a892ac1102bbc64ed
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2024-10-24 15:23:29 +0100

    Tidy up CrossCore classes
    - Remove `AuthApiService->getCredentials()` to prevent secrets from leaking
    - Use `Container->get()` rather than duplicating the AuthApiService build steps
    - Type mocks correctly with MockObject for Psalm
    
    #patch

--- service-api/module/Application/test/ApplicationTest/Services/Experian/FraudApi/ExperianCrosscoreFraudApiServiceTest.php
+++ service-api/module/Application/test/ApplicationTest/Services/Experian/FraudApi/ExperianCrosscoreFraudApiServiceTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace ApplicationTest\ApplicationTest\Services\Experian\FraudApi;
 
-use Application\Experian\Crosscore\AuthApi\DTO\RequestDTO as AuthRequestDTO;
 use Application\Experian\Crosscore\AuthApi\AuthApiService;
 use Application\Experian\Crosscore\FraudApi\DTO\RequestDTO;
 use Application\Experian\Crosscore\FraudApi\FraudApiException;
@@ -13,6 +12,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response as GuzzleResponse;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Throwable;
 
@@ -20,7 +20,7 @@ class ExperianCrosscoreFraudApiServiceTest extends TestCase
 {
     private array $config;
 
-    private AuthApiService $experianCrosscoreAuthApiService;
+    private AuthApiService&MockObject $experianCrosscoreAuthApiService;
 
     public function setUp(): void
     {
@@ -30,13 +30,6 @@ class ExperianCrosscoreFraudApiServiceTest extends TestCase
         ];
 
         $this->experianCrosscoreAuthApiService = $this->createMock(AuthApiService::class);
-    }
-
-    public function testGetCredentials(): void
-    {
-        $credentials = $this->experianCrosscoreAuthApiService->getCredentials();
-
-        $this->assertInstanceOf(AuthRequestDTO::class, $credentials);
     }
 
     /**
@@ -52,9 +45,6 @@ class ExperianCrosscoreFraudApiServiceTest extends TestCase
         if ($expectedException !== null) {
             $this->expectException($expectedException);
         } else {
-            /**
-             * @psalm-suppress UndefinedMethod
-             */
             $this->experianCrosscoreAuthApiService
                 ->expects($this->once())
                 ->method('retrieveCachedTokenResponse');
