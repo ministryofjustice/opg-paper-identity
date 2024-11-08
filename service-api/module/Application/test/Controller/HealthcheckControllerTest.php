9fe1f14a5356d60a9ec9256c6a0ba9cef6f4b50f
John Riordan <jr@spinhalf.com>
2024-11-08 08:56:55 +0000

    ID-393 sonarcloud#patch

--- service-api/module/Application/test/Controller/HealthcheckControllerTest.php
+++ service-api/module/Application/test/Controller/HealthcheckControllerTest.php
@@ -40,7 +40,7 @@ class HealthcheckControllerTest extends TestCase
         $configOverrides = [];
 
         $this->setApplicationConfig(ArrayUtils::merge(
-            include __DIR__ . '/../../../../config/application.config.php',
+            include_once __DIR__ . '/../../../../config/application.config.php',
             $configOverrides
         ));
 
