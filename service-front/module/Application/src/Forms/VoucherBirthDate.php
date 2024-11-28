1121c277ecb2d515a7cece52571cac271a452e79
David Bridgwood <124149420+davidbridgwood@users.noreply.github.com>
2024-11-28 09:01:19 +0000

    Feature/id 377/voucher dob error messages (#323)
    * improve error messages voucher name and dob pages
    
    * update template with new error formats
    
    * test new errors
    
    * formatting actual date input error
    
    * fix cypres test

--- service-front/module/Application/src/Forms/VoucherBirthDate.php
+++ service-front/module/Application/src/Forms/VoucherBirthDate.php
@@ -19,6 +19,8 @@ class VoucherBirthDate
      */
     #[Annotation\Validator(BirthDateValidator::class, options: [
         "messages" => [
+            BirthDateValidator::DATE_EMPTY => "Enter their date of birth",
+            BirthDateValidator::DATE_FORMAT => "Date of birth must be a valid date",
             BirthDateValidator::DATE_18  => "The person vouching must be 18 years or older."
         ]
     ])]
--- service-front/module/Application/src/Forms/VoucherName.php
+++ service-front/module/Application/src/Forms/VoucherName.php
@@ -18,15 +18,15 @@ use Laminas\Validator\NotEmpty;
 #[Annotation\Hydrator(ObjectPropertyHydrator::class)]
 class VoucherName implements FormTemplate
 {
-     /**
+    /**
      * @psalm-suppress PossiblyUnusedProperty
      */
-    #[Annotation\Validator(NotEmpty::class)]
+    #[Annotation\Validator(NotEmpty::class, ['message' => 'Enter their first name(s)'])]
     public mixed $firstName = null;
 
     /**
      * @psalm-suppress PossiblyUnusedProperty
      */
-    #[Annotation\Validator(NotEmpty::class)]
+    #[Annotation\Validator(NotEmpty::class, ['message' => 'Enter their last name'])]
     public mixed $lastName = null;
 }
