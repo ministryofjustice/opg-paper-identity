dba6583bcebbe37b12b3294357c7eb165d3349bf
David Bridgwood <david.bridgwood@digital.justice.gov.uk>
2024-12-16 11:42:27 +0000

    mock data to test add-donor behaviour

--- service-front/module/Application/src/Forms/AddDonor.php
+++ service-front/module/Application/src/Forms/AddDonor.php
@@ -7,17 +7,26 @@ namespace Application\Forms;
 use Application\Validators\LpaUidValidator;
 use Laminas\Form\Annotation;
 use Laminas\Hydrator\ObjectPropertyHydrator;
+use Laminas\Validator\NotEmpty;
 
 /**
  * @psalm-suppress MissingConstructor
  * @implements FormTemplate<array{lpa: string}>
  */
 #[Annotation\Hydrator(ObjectPropertyHydrator::class)]
-class LpaReferenceNumber implements FormTemplate
+class AddDonor implements FormTemplate
 {
     /**
      * @psalm-suppress PossiblyUnusedProperty
      */
     #[Annotation\Validator(LpaUidValidator::class)]
+    // empty values are handled in the LpaUidValidator
+    #[Annotation\Validator(NotEmpty::class, options: [
+        'type' => NotEmpty::ALL & ~NotEmpty::STRING
+    ])]
     public string $lpa;
+
+    public bool $declaration;
+
+    public array $lpas;
 }
