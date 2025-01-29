f8b55164571d1fff361b18b5af437aee12f110c8
Richard M <richard.mohammed@digital.justice.gov.uk>
2025-01-29 15:36:57 +0000

    Remove unneccessary comments

--- service-front/module/Application/src/Validators/BirthDateValidator.php
+++ service-front/module/Application/src/Validators/BirthDateValidator.php
@@ -42,13 +42,11 @@ class BirthDateValidator extends AbstractValidator
     {
         $this->setValue($value);
 
-        // Check for empty date values or invalid placeholder like '--'
         if (empty($this->value) || $this->value === '' || $this->value === '--') {
             $this->error(self::DATE_EMPTY);
             return false;
         }
 
-        // Check if the date is in valid format
         try {
             $dateTime = new \DateTime($this->value);
         } catch (\Exception $exception) {
@@ -56,25 +54,20 @@ class BirthDateValidator extends AbstractValidator
             return false;
         }
 
-        // Check if the date is a real valid date (no impossible dates - eg: 29th Feb, except for leap year)
         if (! $this->isRealDate($value)) {
             $this->error(self::DATE_FORMAT);
             return false;
         }
 
-        // Check if the date is in the future
         $currentDate = new \DateTime();
         if ($dateTime > $currentDate) {
             $this->error(self::DATE_FUTURE);
             return false;
         }
 
-        // Validate that the date represents an age of at least 18
         $birthDate = strtotime($value);
         $minBirthDate = strtotime(self::EIGHTEEN_YEARS, time());
 
-
-        // If the birth date is less than 18 years ago, return an error
         if ($birthDate > $minBirthDate) {
             $this->error(self::DATE_18);
             return false;
