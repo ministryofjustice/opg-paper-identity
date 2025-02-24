4f0d4b29739a1174695cadb57ba83cf76898f7f3
David Bridgwood <david.bridgwood@digital.justice.gov.uk>
2025-02-24 15:24:12 +0000

    consolidate and re-style document-success page

--- service-front/module/Application/config/module.config.php
+++ service-front/module/Application/config/module.config.php
@@ -613,21 +613,21 @@ return [
         'template_options' => [
             'NATIONAL_INSURANCE_NUMBER' => [
                 'default' => 'application/pages/national_insurance_number',
-                'success' => 'application/pages/national_insurance_number_success',
+                'success' => 'application/pages/document_success',
                 'fail' => 'application/pages/national_insurance_number_fail',
                 'thin_file' => 'application/pages/thin_file_failure',
                 'fraud' => 'application/pages/fraud_failure'
             ],
             'PASSPORT' => [
                 'default' => 'application/pages/passport_number',
-                'success' => 'application/pages/passport_number_success',
+                'success' => 'application/pages/document_success',
                 'fail' => 'application/pages/passport_number_fail',
                 'thin_file' => 'application/pages/thin_file_failure',
                 'fraud' => 'application/pages/fraud_failure'
             ],
             'DRIVING_LICENCE' => [
                 'default' => 'application/pages/driving_licence_number',
-                'success' => 'application/pages/driving_licence_number_success',
+                'success' => 'application/pages/document_success',
                 'fail' => 'application/pages/driving_licence_number_fail',
                 'thin_file' => 'application/pages/thin_file_failure',
                 'fraud' => 'application/pages/fraud_failure'
