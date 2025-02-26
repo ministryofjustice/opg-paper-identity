c0f7e29a4e1b3d452f2ce012d0337a9eeeb3f934
Richard M <richard.mohammed@digital.justice.gov.uk>
2025-02-26 14:40:27 +0000

    Court of protection what next page and unit test

--- service-front/module/Application/test/Controller/CourtOfProtectionFlowControllerTest.php
+++ service-front/module/Application/test/Controller/CourtOfProtectionFlowControllerTest.php
@@ -106,4 +106,22 @@ class CourtOfProtectionFlowControllerTest extends AbstractHttpControllerTestCase
         $this->assertResponseStatusCode(302);
         $this->assertRedirectTo("/{$this->uuid}/court-of-protection-what-next");
     }
+
+    public function testWhatNextActionLoadsSuccessfully(): void
+    {
+        $mockResponseData = $this->returnOpgResponseData();
+
+        $this->opgApiServiceMock
+            ->expects(self::once())
+            ->method('getDetailsData')
+            ->with($this->uuid)
+            ->willReturn($mockResponseData);
+
+        $this->dispatch("/{$this->uuid}/court-of-protection-what-next", 'GET');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('application');
+        $this->assertControllerName(CourtOfProtectionFlowController::class);
+        $this->assertControllerClass('CourtOfProtectionFlowController');
+        $this->assertMatchedRouteName('root/court_of_protection_what_next');
+    }
 }
