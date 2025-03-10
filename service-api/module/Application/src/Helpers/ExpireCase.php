e829212d224800f4e2b49155ed8cbffd053a931c
David Bridgwood <david.bridgwood@digital.justice.gov.uk>
2025-03-10 12:07:52 +0000

    sonarcloud fix

--- service-api/module/Application/src/Helpers/ExpireCase.php
+++ service-api/module/Application/src/Helpers/ExpireCase.php
@@ -20,10 +20,10 @@ class ExpireCase
     public function setCaseExpiry(string $uuid): void
     {
 
-        $TTL_DAYS = getenv("AWS_DYNAMODB_TTL_DAYS");
-        $ttl = $this->clock->now()->modify("+{$TTL_DAYS} days");
+        $ttlDays = getenv("AWS_DYNAMODB_TTL_DAYS");
+        $ttl = $this->clock->now()->modify("+{$ttlDays} days");
 
-        $this->logger->info("Setting case {$uuid} to expire in {$TTL_DAYS} days");
+        $this->logger->info("Setting case {$uuid} to expire in {$ttlDays} days");
         $this->dataHandler->setTTL($uuid, $ttl->format('U'));
     }
 }
