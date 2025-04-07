3e735a53fd0d939e7ce7b352a06e39a218d9c365
David Bridgwood <david.bridgwood@digital.justice.gov.uk>
2025-04-07 16:05:56 +0100

    id-559 remove redundant code

--- service-front/module/Application/test/Feature/Controller/IndexControllerTest.php
+++ service-front/module/Application/test/Feature/Controller/IndexControllerTest.php
@@ -25,22 +25,6 @@ class IndexControllerTest extends AbstractHttpControllerTestCase
         $serviceManager->setAllowOverride(true);
     }
 
-    public function testIndexActionCanBeAccessed(): void
-    {
-        $this->dispatch('/', 'GET');
-        $this->assertResponseStatusCode(200);
-        $this->assertModuleName('application');
-        $this->assertControllerName(IndexController::class); // as specified in router's controller name alias
-        $this->assertControllerClass('IndexController');
-        $this->assertMatchedRouteName('root/home');
-    }
-
-    public function testIndexActionViewModelTemplateRenderedWithinLayout(): void
-    {
-        $this->dispatch('/', 'GET');
-        $this->assertQuery('body h1');
-    }
-
     public function testInvalidRouteDoesNotCrash(): void
     {
         $this->dispatch('/invalid/route', 'GET');
