7699620eacfb958b3b6bbe7935d2b610ec0b5c79
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2026-01-16 16:54:00 +0000

    Migrate IndexController to mezzio handlers
    #patch

---
+++ service-front/module/Application/src/Handler/HealthCheck/ServiceStatusHandler.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Handler\HealthCheck;
+
+use Application\Contracts\OpgApiServiceInterface;
+use Application\Services\SiriusApiService;
+use Laminas\Diactoros\Response\JsonResponse;
+use Laminas\Psr7Bridge\Psr7ServerRequest;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class ServiceStatusHandler implements RequestHandlerInterface
+{
+    public function __construct(
+        private readonly SiriusApiService $siriusApiService,
+        private readonly OpgApiServiceInterface $opgApiService,
+    ) {
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $ok = true;
+
+        $laminasRequest = Psr7ServerRequest::toLaminas($request);
+        $siriusResponse = $this->siriusApiService->checkAuth($laminasRequest);
+        if ($siriusResponse !== true) {
+            $ok = false;
+        }
+
+        $apiResponse = $this->opgApiService->healthCheck();
+        if ($apiResponse !== true) {
+            $ok = false;
+        }
+
+        return new JsonResponse([
+            'OK' => $ok,
+            'dependencies' => [
+                'sirius' => [
+                    'ok' => $siriusResponse,
+                ],
+                'api' => [
+                    'ok' => $apiResponse,
+                ],
+            ],
+        ]);
+    }
+}
